f5f8eface899d3656adbe6cc711cbfea88276219
Marcio Eduardo Felix <32266467+felixskmarcio@users.noreply.github.com>
2025-03-30 17:47:14 -0300

    Adiciona configuração do Vite

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+    host: true,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {}
+      }
+    }
+  }
+});
