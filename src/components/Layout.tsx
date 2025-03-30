90dc124fc643c7bdea0e9aae2e324701dda12d4b
Marcio Eduardo Felix <32266467+felixskmarcio@users.noreply.github.com>
2025-03-30 17:48:08 -0300

    Adiciona componente de Layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import Header from './Header';
+
+export default function Layout() {
+  return (
+    <div className="flex h-screen bg-gray-100 dark:bg-gray-900">
+      {/* Sidebar */}
+      <Sidebar />
+      
+      {/* Main Content */}
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-auto p-4">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
